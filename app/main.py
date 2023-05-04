f679e2e2e60655137ac8c39bc9a224138db71c70
Abdelrhman-Abdelaziz <85349614+Abdelrhman-Abdelaziz@users.noreply.github.com>
2023-05-04 18:00:48 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from Questiongen import main
-
+from Questiongen import distrctors
+import random
 app = FastAPI()
 qg = main.QGen()
 
@@ -14,26 +15,56 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 class QuestionRequest(BaseModel):
     context: str
-    n_ques:int
+    n_ques: int
     n_mcq: int
+
 
 class QuestionResponse(BaseModel):
     questions: list
 
 
+class sentenceQuestionRequest(BaseModel):
+    sentence: str
+
+
+class sentenceQuestionResponse(BaseModel):
+    question: str
+    mcq: list
+    answer: str
+
+
 @app.get('/')
 def index():
-    return {'message':'hello world',"Author":"Abdelrhman Abdelaziz"}
+    return {'message': 'hello world', "Author": "Abdelrhman Abdelaziz"}
+
 
 @app.post("/mcq", response_model=QuestionResponse)
-def getquestion(request: QuestionRequest):
+def getquestions(request: QuestionRequest):
     context = request.context
     n_mcq = request.n_mcq
     n_ques = request.n_ques
-    if n_ques <= 0 or n_mcq <=0 :
+    if n_ques <= 0 or n_mcq <= 0:
         return QuestionResponse(questions=[])
-    ques = qg.predict_mcq(context,n_ques,n_mcq)
+    ques = qg.predict_mcq(context, n_ques, n_mcq)
     return QuestionResponse(questions=ques)
 
+
+@app.post("/fillBlank", response_model=sentenceQuestionResponse)
+def getFillBlank(request: sentenceQuestionRequest):
+    keywords = qg.get_nouns_multipartite(request.sentence)
+    keywords = distrctors.filter_keywords(keywords, qg.s2v)
+    if len(keywords) > 0:
+        raise HTTPException(status_code=400, detail="Can't Make question")
+    random.shuffle(keywords)
+    answer = keywords[0]
+    question = request.sentence.replace(answer, " ....... ")
+    mcq = distrctors.get_distractors(answer,question,qg.s2v,qg.s_t_model,30,0.2)[:3]
+
+    mcq.append(answer)
+    random.shuffle(mcq)
+
+    return sentenceQuestionResponse(question= question, mcq= mcq ,answer= answer)
